6edec360d6be285b07eda57cb9035332b433954f
apptreeso <superwebdev19@gmail.com>
2023-09-21 22:08:14 +0900

    update task index

--- tasks/index.ts
+++ tasks/index.ts
@@ -2,3 +2,7 @@ import "./deploy-comptroller";
 import "./deploy-ctoken";
 import "./deploy-price-oracle";
 import "./support-markets";
+import "./sync-collateral-factors";
+import "./sync-reserve-factors";
+import "./update-price-oracle";
+
