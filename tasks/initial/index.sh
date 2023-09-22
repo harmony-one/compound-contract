d29723cc14d7cb24fa38bbeafbdc791fb1af5f4d
apptreeso <superwebdev19@gmail.com>
2023-09-22 11:51:53 +0900

    update configurations

--- tasks/initial/index.sh
+++ tasks/initial/index.sh
@@ -1,46 +1,54 @@
 #!/bin/(shell)
 
 # NETWORK=mainnet \
-# OWNER=0xfb59ce8986943163f14c590755b29db2998f2322 \
+# OWNER=0xD0c46E5e40b38eEB3b685fD18550fC54e5aFA0F2 \
 # sh ./tasks/initial/index.sh
 
 npx hardhat deploy --network $NETWORK
 
 npx hardhat deploy-ctoken \
 --network $NETWORK \
---underlying-address 0x4200000000000000000000000000000000000006 \
+--underlying-address 0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a \
 --underlying-decimals 18 \
---underlying-name "Wrapped Ether" \
---underlying-symbol "WETH" \
+--underlying-name "Wrapped ONE" \
+--underlying-symbol "WONE" \
 --decimals 8 \
 --comptroller-key "Unitroller" \
 --interest-rate-model-key "MediumRateModel" \
---owner $OWNER \
---proxy true
+--owner $OWNER
 
 npx hardhat deploy-ctoken \
 --network $NETWORK \
---underlying-address 0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca \
---underlying-decimals 6 \
---underlying-name "USD Base Coin" \
---underlying-symbol "USDbC" \
---decimals 8 \
---comptroller-key "Unitroller" \
---interest-rate-model-key "StableRateModel" \
---owner $OWNER \
---proxy true
-
-npx hardhat deploy-ctoken \
---network $NETWORK \
---underlying-address 0x50c5725949a6f0c72e6c4a641f24049a917db0cb \
+--underlying-address 0x4cC435d7b9557d54d6EF02d69Bbf72634905Bf11 \
 --underlying-decimals 18 \
---underlying-name "Dai Stablecoin" \
---underlying-symbol "DAI" \
+--underlying-name "ETH" \
+--underlying-symbol "1ETH" \
+--decimals 8 \
+--comptroller-key "Unitroller" \
+--interest-rate-model-key "MediumRateModel" \
+--owner $OWNER
+
+npx hardhat deploy-ctoken \
+--network $NETWORK \
+--underlying-address 0xBC594CABd205bD993e7FfA6F3e9ceA75c1110da5 \
+--underlying-decimals 6 \
+--underlying-name "USD Coin" \
+--underlying-symbol "1USDC" \
 --decimals 8 \
 --comptroller-key "Unitroller" \
 --interest-rate-model-key "StableRateModel" \
---owner $OWNER \
---proxy true
+--owner $OWNER
+
+npx hardhat deploy-ctoken \
+--network $NETWORK \
+--underlying-address 0xF2732e8048f1a411C63e2df51d08f4f52E598005 \
+--underlying-decimals 6 \
+--underlying-name "Tether USD" \
+--underlying-symbol "1USDT" \
+--decimals 8 \
+--comptroller-key "Unitroller" \
+--interest-rate-model-key "StableRateModel" \
+--owner $OWNER
 
 
 npx hardhat deploy-price-oracle --network $NETWORK
