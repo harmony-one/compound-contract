92596da360232c8e0621d42a26a5b137c6118282
apptreeso <superwebdev19@gmail.com>
2023-09-24 20:15:53 +0900

    add the protocol config

---
+++ types.ts
@@ -0,0 +1,19 @@
+export type ProtocolConfig = {
+  [network: string]: ProtocolNetworkConfig;
+};
+
+export type ProtocolNetworkConfig = {
+  timelock?: `0x${string}`;
+  multisig?: `0x${string}`;
+  markets: {
+      [symbol: string]: MarketConfig;
+  };
+};
+
+export type MarketConfig = {
+  source: "chainlink";
+  priceFeed: `0x${string}`;
+  baseUnit: `1${string}`;
+  reserveFactor?: number;
+  collateralFactor?: number;
+};
