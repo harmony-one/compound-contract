094632351638a4ae299c64444b8d8facfae82a8f
apptreeso <superwebdev19@gmail.com>
2023-09-25 11:12:02 +0900

    add the task to upgrade comptroller on the index

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,8 +1,8 @@
 import "./deploy-comptroller";
+import "./upgrade-comptroller";
 import "./deploy-ctoken";
 import "./deploy-price-oracle";
 import "./support-markets";
 import "./sync-collateral-factors";
 import "./sync-reserve-factors";
 import "./update-price-oracle";
-
